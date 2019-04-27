65603184105081057c6a021732404c21e1ec4737
Tyler Hancock <darklime@live.ca>
2019-04-27 13:41:03 -0600

    Set FPS count to 240. Likely not our target FPS, but it's a high end one.

--- desktop/src/org/epoxide/ld44/desktop/DesktopLauncher.java
+++ desktop/src/org/epoxide/ld44/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import org.epoxide.ld44.LD44;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.backgroundFPS = 240;
+        config.foregroundFPS = 240;
 		new LwjglApplication(new LD44(), config);
 	}
 }
