7dcd6fbf5607a74ed5c3b3a972b4051a4cbe69ea
lclc98 <liam.c14785@gmail.com>
2019-04-27 11:46:20 +1000

    Initial commit

---
+++ desktop/src/org/epoxide/ld44/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.epoxide.ld44.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.epoxide.ld44.LD44;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD44(), config);
+	}
+}
